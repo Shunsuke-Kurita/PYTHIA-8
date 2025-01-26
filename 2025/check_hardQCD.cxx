06645b412069b36a1e5811469202208c33c73d1c
Shunsuke-Kurita <kurita.quark@gmail.com>
2025-01-26 09:28:43 +0900

    Template function of calculating Delta Phi

--- 2025/check_hardQCD.cxx
+++ 2025/check_hardQCD.cxx
@@ -4,7 +4,16 @@ using namespace Pythia8;
 #include "TH1F.h"
 #include "TFile.h"
 
-// Charm and bottom pair creations by gluon fusion
+template <typename T>
+double calculateDeltaPhi(const T& event, int iParticle1, int iParticle2) {
+    double deltaPhi = event[iParticle1].phi() - event[iParticle2].phi();
+    if (deltaPhi > 3 * M_PI / 2) {
+        deltaPhi -= 2 * M_PI;
+    } else if (deltaPhi < -M_PI / 2) {
+        deltaPhi += 2 * M_PI;
+    }
+    return deltaPhi;
+}
 
 int main() {
 
@@ -19,34 +28,31 @@ int main() {
     TH1F* eta = new TH1F("Pseudorapidity", ";#eta;Entries", 100, -5., 5.);
     TH1F* phi = new TH1F("Azimuthal angle", ";#phi (rad);Entries", 600, - M_PI, M_PI);
     TH1F* hDeltaPhi = new TH1F("hDeltaPhi", "Charm - AntiCharm;#Delta#phi (rad);Entries", 600, -0.5 * M_PI, 1.5 * M_PI);
+    // TH1F* hDeltaPhi = new TH1F("hDeltaPhi", "Bottom - AntiBottom;#Delta#phi (rad);Entries", 600, -0.5 * M_PI, 1.5 * M_PI);
 
     for (int iEvent = 0; iEvent < 10000; ++iEvent) {
         pythia.next();
 
-        int iCharm = 0, iAntiCharm = 0;
+        int iParticle = 0, iAntiParticle = 0;
+
         for (int i = 0; i < pythia.event.size(); ++i) {
-            if (pythia.event[i].id() == 4) iCharm = i;
-            if (pythia.event[i].id() == -4) iAntiCharm = i;
+            if (pythia.event[i].id() == 4) iParticle = i;
+            if (pythia.event[i].id() == -4) iAntiParticle = i;
+            // if (pythia.event[i].id() == 5) iParticle = i;
+            // if (pythia.event[i].id() == -5) iAntiParticle = i;
         }
 
-        double kDeltaPhiPair = 0.;
-        double kDeltaPhi = pythia.event[iCharm].phi() - pythia.event[iAntiCharm].phi();
-        if (kDeltaPhi  > 3 * M_PI / 2) {
-            kDeltaPhiPair = kDeltaPhi  - 2 * M_PI;
-        } else if (kDeltaPhi  < -M_PI / 2) {
-            kDeltaPhiPair = kDeltaPhi  + 2 * M_PI;
-        } else {
-            kDeltaPhiPair = kDeltaPhi;
-        }
+        if (iParticle != 0 && iAntiParticle != 0) {
+            double deltaPhiPair = calculateDeltaPhi(pythia.event, iParticle, iAntiParticle);
 
-        pT->Fill(pythia.event[iCharm].pT());
-        eta->Fill(pythia.event[iCharm].eta());
-        phi->Fill(pythia.event[iCharm].phi());
-        hDeltaPhi->Fill(kDeltaPhiPair);
+            pT->Fill(pythia.event[iParticle].pT());
+            eta->Fill(pythia.event[iParticle].eta());
+            phi->Fill(pythia.event[iParticle].phi());
+            hDeltaPhi->Fill(deltaPhiPair);
+        }
         
     }
 
-    // cout << pT << eta << phi;
     TFile* outFileRoot = new TFile("output.root", "RECREATE");
     pT->Write();
     eta->Write();
